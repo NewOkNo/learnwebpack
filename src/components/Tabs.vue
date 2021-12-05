786791c2cf7ab5accb43ab3097ebfd33888cc6d0
NewOkNo <andrejshersh@gmail.com>
2021-12-05 18:55:47 +0200

    reusable tabs added

---
+++ src/components/Tabs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tabs">
+    <ul>
+      <li v-for="tab in tabArray" :key="tab.id" :class="{'is-active':tab.active}" @click="$emit('tabclick', tab.id)"><a>{{ tab.name }}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tabArray']
+}
+</script>
+
+<style>
+
+</style>
