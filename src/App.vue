324700b70baa150c46b273b1c9c264ed61bbbfb1
Andrei Šeršukov <TA-19V-633@tptlive.ee>
2021-12-06 16:00:40 +0200

    ws

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,44 @@
 <template>
-<div>
-  <!-- <todo-example></todo-example> -->
-    <!--<button class="button is-primary" @click="active=true">Click me</button>
-    <button class="button is-primary" @click="active2=true">Click me 2</button>
-    <modal v-if="active" @close="active=false" img="http://placekitten.com/800/600"></modal>
-    <modal v-show="active2" @close="active2=false" img="http://placebear.com/800/600"></modal>-->
-    <tabs :tabArray="tabArray" @tabclick="changeTab($event, tabArray)"></tabs>
-    <tabs :tabArray="tabArray2" @tabclick="changeTab($event, tabArray2)"></tabs>
-</div>
+  <div class="container">
+      <input type="text" class="input" v-model="message" @keydown.enter="sendMessage">
+      <ul>
+          <li v-for="(message, key) in messages" :key="key" class="mt-3">
+              <span class="tag is-primary is-light is-size-3">{{ message }}</span>
+          </li>
+      </ul>
+  </div>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
-import Tabs from './components/Tabs.vue'
-import TodoExample from './components/TodoExample.vue'
 export default {
-    components: { TodoExample, Modal, Tabs },
-    data() {
-        return {
-            active: false,
-            active2: false,
-            tabArray: [
-                {
-                    id: 0,
-                    name: 'tab1',
-                    active: true
-                },
-                {
-                    id: 1,
-                    name: 'tab2',
-                    active: false
-                }
-            ],
-            tabArray2: [
-                {
-                    id: 0,
-                    name: 'tab1',
-                    active: true
-                },
-                {
-                    id: 1,
-                    name: 'tab2',
-                    active: false
-                },
-                {
-                    id: 2,
-                    name: 'tab3',
-                    active: false
-                }
-            ]
+    created(){
+        // Create WebSocket connection.
+        this.ws = new WebSocket('ws://localhost:3333');
+
+        // Connection opened
+        this.ws.addEventListener('open', this.onOpen);
+
+        // Listen for messages
+        this.ws.addEventListener('message', this.onMessage);
+    },
+    data(){
+        return{
+            ws: null,
+            message: '',
+            messages: []
         }
     },
     methods: {
-        changeTab(id, tabArray) {
-            tabArray.forEach(tab => tab.active = tab.id!==id ? false : true)
+        onOpen(event){
+            this.ws.send('Hello Server!');
+        },
+        onMessage(event) {
+            console.log('Message from server ', event.data);
+            this.messages.push(event.data);
+        },
+        sendMessage(){
+            this.ws.send(this.message);
+            this.message = '';
         }
     }
 }
---
+++ src/Appold2.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="columns">
+            <div class="column is-one-quarter" v-for="post in posts" :key="post.id">
+                <post-card :post="post"></post-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import PostCard from './components/PostCard.vue';
+const axios = require('axios');
+export default {
+  components: { PostCard },
+    created(){
+        axios.get('http://127.0.0.1:8000/api/posts').then(response => {
+            console.log(response);
+            this.posts = response.data;
+        });
+    },
+    data() {
+        return{
+            posts: []
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
